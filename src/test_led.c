ac198f7b471b606573974f96a97ed6bb8dc58413
Evgeniy <Evgeniy>
2018-09-17 22:49:29 +0300

    fix with acc. to C51 spec

--- src/test_led.c
+++ src/test_led.c
@@ -1,5 +1,6 @@
 #include "aduc812.h"
 #include "led.h"
+#include "max.h"
 
 
 void delay ( unsigned long ms ) {
@@ -11,7 +12,8 @@ void delay ( unsigned long ms ) {
 
 unsigned char binary_from_string(char* s) {
   unsigned char result = 0;
-  for (int i = 0; i < 8; ++i )
+  signed char i;
+  for (i = 0; i < 8; ++i)
       result |= (s[i] == '1') << (7 - i);
   return result; 
 }
@@ -29,20 +31,20 @@ unsigned char read_dip() {
 void main( void ) {
     unsigned char left_leds = binary_from_string("11000000");
     unsigned char right_leds = binary_from_string("00000001");
-    unsigned char leds = 0;
+    unsigned char leds_var = 0;
     unsigned char dip = 0;
     while( 1 ) {
         delay(20);
         dip = read_dip();
         if (dip == 0x11) {
-            leds = left_leds | right_leds;
+            leds_var = left_leds | right_leds;
             left_leds = right_circular_shift(left_leds, 1);
             right_leds = left_circular_shift(right_leds, 1);
         } else {
             left_leds = binary_from_string("11000000");
             right_leds = binary_from_string("00000001");
-            leds = dip;
+            leds_var = dip;
         }
-        leds(leds);
+        leds(leds_var);
     }
 }    
